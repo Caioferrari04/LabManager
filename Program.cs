cad3518ae520d1271d478d8b17530eee806f9ab2
Caioferrari04 <caioferrari0484@gmail.com>
2022-05-14 12:10:43 -0300

    Adicionando tabela lab

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ command.CommandText = @"
         ID int not null primary key,
         ram varchar(100) not null,
         processor varchar(100) not null
+    );
+    CREATE TABLE IF NOT EXISTS Labs(
+        ID INT NOT NULL PRIMARY KEY,
+        Number INT NOT NULL,
+        Name VARCHAR(100) NOT NULL,
+        Block VARCHAR(50) NOT NULL
     );";
 
 command.ExecuteNonQuery();
@@ -29,7 +35,7 @@ if (modelName == "Computer")
 
         command.CommandText = $"SELECT * FROM Computers;";
 
-        var reader = command.ExecuteReader(); 
+        var reader = command.ExecuteReader();
 
         while (reader.Read())
         {
@@ -56,5 +62,55 @@ if (modelName == "Computer")
         command.ExecuteNonQuery();
 
         connection.Close();
+    }
+}
+else if (modelName == "Lab")
+{
+    switch (modelAction)
+    {
+        case "New":
+            {
+                var id = Convert.ToInt32(args[2]);
+                var number = args[3];
+                var name = args[4];
+                var block = args[5];
+                connection = new SqliteConnection("Data Source=database.db");
+                connection.Open();
+                command = connection.CreateCommand();
+
+                command.CommandText = $"INSERT INTO Labs VALUES($id, $number, $name, $block)";
+
+                command.Parameters.AddWithValue("$id", id);
+                command.Parameters.AddWithValue("$number", number);
+                command.Parameters.AddWithValue("$name", name);
+                command.Parameters.AddWithValue("$block", block);
+
+                command.ExecuteNonQuery();
+
+                connection.Close();
+
+                break;
+            }
+        case "List":
+            {
+                connection = new SqliteConnection("Data Source=database.db");
+                connection.Open();
+                command = connection.CreateCommand();
+
+                command.CommandText = $"SELECT * FROM Labs;";
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader.GetInt32(0)} {reader.GetString(1)} {reader.GetString(2)} {reader.GetString(3)}");
+                }
+
+                connection.Close();
+                break;
+            }
+        default:
+            Console.WriteLine("Comando inválido");
+            break;
     }
 }
