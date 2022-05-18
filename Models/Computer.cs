f9609332c55bca03d4359d3b720c7f1ca6b6109f
Caioferrari04 <caioferrari0484@gmail.com>
2022-05-18 11:55:05 -0300

    refactor: isolando criação e listagem de dados

---
+++ Models/Computer.cs
@@ -0,0 +1,16 @@
+namespace LabManager.Models;
+
+public class Computer 
+{
+    public Computer(int id, string? ram, string? processor)
+    {
+        ID = id;
+        Ram = ram;
+        Processor = processor;
+    }
+    public int ID { get; set; }
+    
+    public string? Ram { get; set; }
+    
+    public string? Processor { get; set; }
+}
---
+++ Repositories/ComputerRepository.cs
@@ -0,0 +1,33 @@
+using LabManager.Models;
+using Microsoft.Data.Sqlite;
+
+namespace LabManager.Repositories;
+
+public class ComputerRepository 
+{
+    public List<Computer> GetAll() 
+    {
+        var computers = new List<Computer>();
+
+        var connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = $"SELECT * FROM Computer;";
+
+        var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            var id = reader.GetInt32(0);
+            var ram = reader.GetString(1);
+            var processor = reader.GetString(2);
+
+            var computer = new Computer(id: id, ram: ram, processor: processor);
+            computers.Add(computer); 
+        }
+
+        connection.Close();
+
+        return computers;
+    }
+}
