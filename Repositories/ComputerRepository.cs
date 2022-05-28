8b1eb546436f556815afe5a83ad00d9c85de0947
Caioferrari04 <caioferrari0484@gmail.com>
2022-05-27 21:08:59 -0300

    Adicionando método para atualizar computador

--- Repositories/ComputerRepository.cs
+++ Repositories/ComputerRepository.cs
@@ -54,7 +54,7 @@ public class ComputerRepository
         connection.Close();
     }
 
-    public Computer GetById(int id) 
+    public Computer GetById(int id)
     {
         var connection = new SqliteConnection(_databaseConfig.ConnectionString);
         connection.Open();
@@ -64,13 +64,32 @@ public class ComputerRepository
         command.Parameters.AddWithValue("$id", id);
 
         var reader = command.ExecuteReader();
-        
+
         reader.Read();
         var ram = reader.GetString(1);
         var processor = reader.GetString(2);
 
         var computer = new Computer(id: id, ram: ram, processor: processor);
-        
+
+        connection.Close();
+
+        return computer;
+    }
+    public Computer Update(Computer computer)
+    {
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
+
+
+        var command = connection.CreateCommand();
+
+        command.CommandText = $"UPDATE Computer SET Ram=$Ram, Processor=$Processor WHERE ID=$ID;";
+        command.Parameters.AddWithValue("$ID", computer.ID);
+        command.Parameters.AddWithValue("$Ram", computer.Ram);
+        command.Parameters.AddWithValue("$Processor", computer.Processor);
+
+        command.ExecuteNonQuery();
+
         connection.Close();
 
         return computer;
