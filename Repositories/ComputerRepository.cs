120e600b2d0259d0f45f78358ae9b65aa617f3e9
Caioferrari04 <caioferrari0484@gmail.com>
2022-05-27 21:25:45 -0300

    Adicionando método para excluir computadores por ID

--- Repositories/ComputerRepository.cs
+++ Repositories/ComputerRepository.cs
@@ -94,4 +94,20 @@ public class ComputerRepository
 
         return computer;
     }
+
+    public void Delete(int id) 
+    {
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
+
+
+        var command = connection.CreateCommand();
+
+        command.CommandText = $"DELETE FROM Computer WHERE ID=$ID;";
+        command.Parameters.AddWithValue("$ID", id);
+
+        command.ExecuteNonQuery();
+
+        connection.Close();
+    }
 }
